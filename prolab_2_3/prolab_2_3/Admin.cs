2bbb96c3c4f579b78b57c13e231e9c4118fde389
alperentan <56079264+alperentan@users.noreply.github.com>
2021-05-19 14:38:37 +0300

    Add files via upload

---
+++ prolab_2_3/prolab_2_3/Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prolab2_3Spo
+{
+    public class Admin
+    {
+        public int adminId;
+        public string adminName;
+        public string adminPass;
+
+        public Admin()
+        {
+        }
+
+        public Admin(int adminId, string adminName, string adminPass)
+        {
+            this.adminId = adminId;
+            this.adminName = adminName;
+            this.adminPass = adminPass;
+        }
+    }
+
+
+}
---
+++ prolab_2_3/prolab_2_3/Album.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prolab2_3Spo
+{
+    public class Album
+    {
+        public int albumId;
+        public string albumName;
+        public string type;
+        public string albumDate;
+        public List<Songs> albumSongs = new List<Songs>();
+        public List<Singer> albumSinger = new List<Singer>();
+
+        public Album()
+        {
+        }
+
+        public Album(int albumId, string albumName, string type, string albumDate, List<Songs> albumSongs, List<Singer> albumSinger)
+        {
+            this.albumId = albumId;
+            this.albumName = albumName;
+            this.type = type;
+            this.albumDate = albumDate;
+            this.albumSongs = albumSongs;
+            this.albumSinger = albumSinger;
+        }
+    }
+}
